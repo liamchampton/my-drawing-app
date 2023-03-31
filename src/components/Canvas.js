0b3fdabd6bd79b2bfec43cdc6950e90b29355683
Liam Conroy Hampton <liam.hampton@hotmail.co.uk>
2023-03-31 11:49:50 +0000

    add brush size slider

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -3,6 +3,7 @@ import { useRef, useState } from 'react';
 const DrawingCanvas = () => {
   const canvasRef = useRef(null);
   const [isDrawing, setIsDrawing] = useState(false);
+  const [brushSize, setBrushSize] = useState(10);
 
   const startDrawing = (event) => {
     setIsDrawing(true);
@@ -19,6 +20,7 @@ const DrawingCanvas = () => {
     if (!isDrawing) return;
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
+    ctx.lineWidth = brushSize;
     ctx.lineTo(
       event.clientX - canvas.offsetLeft,
       event.clientY - canvas.offsetTop
@@ -30,16 +32,33 @@ const DrawingCanvas = () => {
     setIsDrawing(false);
   };
 
+  const handleBrushSizeChange = (event) => {
+    setBrushSize(event.target.value);
+  };
+
   return (
-    <canvas
-      ref={canvasRef}
-      width={600}
-      height={400}
-      onMouseDown={startDrawing}
-      onMouseMove={draw}
-      onMouseUp={stopDrawing}
-      onMouseOut={stopDrawing}
-    />
+    <div>
+      <canvas
+        ref={canvasRef}
+        width={600}
+        height={400}
+        onMouseDown={startDrawing}
+        onMouseMove={draw}
+        onMouseUp={stopDrawing}
+        onMouseOut={stopDrawing}
+      />
+      <div>
+        <label htmlFor="brush-size">Brush Size:</label>
+        <input
+          id="brush-size"
+          type="range"
+          min="1"
+          max="50"
+          value={brushSize}
+          onChange={handleBrushSizeChange}
+        />
+      </div>
+    </div>
   );
 };
 
